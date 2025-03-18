1dfda3ffde9c4ec7954c697dba453d74a57aa90e
shaddon meyer <smeyer29@amarillocollege.com>
2025-03-18 14:01:40 -0500

    changes

---
+++ main.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+class Main {
+    
+    public static void main(String[] args)
+    { Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter username");
+    
+        String userName = myObj.nextLine();  // Read user input
+        System.out.println("Username is: " + userName);  // Output user input}
+System.out.println( "what is your year of birth?");
+int year = myObj.nextInt();
+int age = 2025 - year;
+System.out.println("you are" + age + "years old");
+String integer = "2324";
+int int1 = Integer.parseInt(userName);
+System.out.println(int1);
+
+    }}
